b878f2e87054c8e70dab13c4cbc9d6d026f16280
Marcos Antonio Cerqueira <marcosfullstack@gmail.com>
2023-08-11 20:25:16 -0300

    desafio concluido

--- src/helpers/numeros.js
+++ src/helpers/numeros.js
@@ -1,12 +1,12 @@
-function formatarMoeda(quantia,localidade,moeda){
-    return new Intl.NumberFormat(localidade, { style: 'currency', currency: moeda }).format(quantia);
+function formatarMoeda(quantia, localidade, moeda) {
+	return new Intl.NumberFormat(localidade, { style: 'currency', currency: moeda }).format(quantia);
 }
 
-function formatarMoedaParaBRL(amount){
-   return formatarMoeda(amount,'pt-BR','BRL')
+function formatarMoedaParaBRL(amount) {
+	return formatarMoeda(amount, 'pt-BR', 'BRL')
 }
 
 export {
-    formatarMoeda,
-    formatarMoedaParaBRL
+	formatarMoeda,
+	formatarMoedaParaBRL
 }
